5405179dac46a98ae298d3761339636b4bc642b9
DUNYU LIU <dunyuliu@gmail.com>
2024-04-11 10:21:11 +0800

    update installation sh scripts

--- installDepreciatedGMPE-SMTK.sh
+++ installDepreciatedGMPE-SMTK.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/GEMScienceTools/gmpe-smtk
 cd gmpe-smtk
 git checkout 4f008173e89f6e4ba4450fb43e95ffdf51a7c2ba^
+cd ..
 #now add gmpe-smtk to your python path
 #export PYTHONPATH=$PYTHONPATH:$(pwd)
 ##you could also consider adding it to your ~.bashrc
--- setupDr4gm.ubuntu.sh
+++ setupDr4gm.ubuntu.sh
@@ -1,3 +1,9 @@
+#! /bin/bash
+
 apt-get update
 apt-get install vim git python-is-python3 python3-pip
 pip install numpy scipy
+
+chmod -R 755 utils
+bash installDepreciatedGMPE-SMTK.sh
+

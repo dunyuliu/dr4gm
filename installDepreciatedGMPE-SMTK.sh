d5f343c35d16cad1fd72681967de97f2229a33f5
DUNYU LIU <43709243+dunyuliu@users.noreply.github.com>
2024-10-23 16:25:26 -0500

    Update installDepreciatedGMPE-SMTK.sh

--- installDepreciatedGMPE-SMTK.sh
+++ installDepreciatedGMPE-SMTK.sh
@@ -2,6 +2,9 @@
 git clone https://github.com/GEMScienceTools/gmpe-smtk
 cd gmpe-smtk
 git checkout 4f008173e89f6e4ba4450fb43e95ffdf51a7c2ba^
+cd smtk
+sed -i '' 's/cumtrapz/cumulative_trapezoid/g' *.py # cumtrapz is depreciated from scipy as of 20241023.
+cd ..
 cd ..
 #now add gmpe-smtk to your python path
 #export PYTHONPATH=$PYTHONPATH:$(pwd)
